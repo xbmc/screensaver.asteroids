396bd620d19a336f298740a7d4642b293a3f6c59
Alwin Esch <alwin.esch@web.de>
2018-10-06 13:33:37 +0200

    Remove not needed CConfig class

--- src/main.h
+++ src/main.h
@@ -35,28 +35,6 @@ typedef struct TRenderVertex
 
 ////////////////////////////////////////////////////////////////////////////
 //
-class CConfig
-{
-public:
-  int m_NumColumns;    // Number of character columns
-  int m_NumRows;      // Number of character rows
-
-  CRGBA m_CharCol;
-  f32 m_FadeSpeedMin;    // How quickly the characters fade out
-  f32 m_FadeSpeedMax;
-  f32 m_CharDelayMin;    // How long we wait before we add a new char
-  f32 m_CharDelayMax;
-
-  CVector2 m_CharSizeTex;    // Size of the characters in the texture
-  int m_NumChars;      // Number of characters in the texture
-
-  void SetDefaults();
-  void LoadSettings();
-};
-
-
-////////////////////////////////////////////////////////////////////////////
-//
 class CRenderD3D
 {
 public:
@@ -89,7 +67,6 @@ public:
 
 /***************************** G L O B A L S *******************************/
 
-extern  CConfig     gConfig;
 extern  CRenderD3D  gRender;
 
 /***************************** I N L I N E S *******************************/
